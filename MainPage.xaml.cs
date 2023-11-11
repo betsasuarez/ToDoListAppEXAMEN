ff28d010ee493297fbf5ae7c1a1fd19209a05018
CORREA <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-11-10 21:47:49 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ToDoListApp
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<Tasks> Tareas { get; set; } = new ObservableCollection<Tasks>();
+
+        public Tasks MiTarea { get; set; }= new Tasks();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CreateOneTask();
+            BindingContext = this;
+        }
+
+        private void CreateOneTask()
+        {
+            var tarea = new Tasks()
+            {
+                TaskName = "Comprar Pan",
+                Status = "Pendiente",
+                ColorText = "Blue"
+
+            };
+            Tareas.Add(tarea);
+
+        }
+        private void AddBtn_Clicked(object sender, EventArgs e)
+        {
+            Tareas.Add(MiTarea);
+           
+        }
+    }
+}
